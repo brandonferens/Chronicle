16b6b3269ee88b7c2e93f2235fa34b61cc91ae69
Egemen Berker Kızılcan <egemen@kenarkose.com>
2015-08-23 13:11:03 +0300

    Fixed facade namespace and updated version number

--- src/ChronicleFacade.php
+++ src/ChronicleFacade.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Kenarkose\Transit\Facade;
+namespace Kenarkose\Chronicle;
 
 
 use Illuminate\Support\Facades\Facade;
--- src/ChronicleServiceProvider.php
+++ src/ChronicleServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class ChronicleServiceProvider extends ServiceProvider {
 
-    const version = '1.0.0';
+    const version = '1.0.1';
 
     /**
      * Register the service provider.
