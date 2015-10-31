b5e3a55053a788bd046598e3403a04c06bd9eeb5
Egemen Berker Kızılcan <egemen@kenarkose.com>
2015-10-31 22:44:07 +0300

    Improved migration

--- src/ChronicleServiceProvider.php
+++ src/ChronicleServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class ChronicleServiceProvider extends ServiceProvider {
 
-    const version = '1.0.4';
+    const version = '1.0.5';
 
     /**
      * Register the service provider.
