67bdaef38f041f140a719c7e1679ccdc73aa1578
Egemen Berker Kızılcan <egemen@kenarkose.com>
2016-03-18 14:13:57 +0200

    Updated version number

--- src/ChronicleServiceProvider.php
+++ src/ChronicleServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 
 class ChronicleServiceProvider extends ServiceProvider {
 
-    const version = '1.1.0';
+    const version = '1.1.3';
 
     /**
      * Indicates if loading of the provider is deferred.
